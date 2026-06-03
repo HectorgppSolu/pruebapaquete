446e5010a19224fe7762d4928d9b6df91ca993b9
unknown <hectorcortescorp@gmail.com>
2026-06-03 16:57:20 -0600

    1.5

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
-import  { Updater}  from "./components/Updater.tsx";
+import   Updater  from "./components/Updater.tsx";
 
 function App() {
 
